9338b758be4b3c9c72de80f0e9e8719a82d9b6be
almogg <almog.geva@audiocodes.com>
2022-04-15 15:05:36 +0300

    deleted rectangle draw in width_calc function

--- ODV_webcam_detection.py
+++ ODV_webcam_detection.py
@@ -179,9 +179,7 @@ def calculate_ref_image_object_width(imagePath : str, wantedObjectName : str):
             xmin = int(max(1,(boxes[i][1] * imW)))
             ymax = int(min(imH,(boxes[i][2] * imH)))
             xmax = int(min(imW,(boxes[i][3] * imW)))
-            
-            #object big rectangle
-            cv2.rectangle(frame, (xmin,ymin), (xmax,ymax), (10, 255, 0), 2)           
+                                 
             object_name = labels[int(classes[i])] # Look up object name from "labels" array using class index
 
             print("detected object name: " , object_name)
